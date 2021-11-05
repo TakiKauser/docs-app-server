10c9e648ab154913de88ba4c860385e65fd51eca
TakiKauser <takikauserbadhon@gmail.com>
2021-11-06 01:32:05 +0600

    post-toDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
@@ -6,11 +7,30 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
-const app = express();
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sydng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('docsApp');
+        const appointmentCollection = database.collection('appointments');
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            res.json(result);
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Docs App is Running...');
