0e074b3aee7e45503e4b065ddc671ed1da48baf3
TakiKauser <takikauserbadhon@gmail.com>
2021-11-15 22:44:06 +0600

    token

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
+
+const serviceAccount = require('./docs-app-kb-firebase-adminsdk-p8868-2829ac0e1f.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 // middleware
 app.use(cors());
@@ -12,6 +19,19 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sydng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        } catch (error) {
+
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -67,7 +87,7 @@ async function run() {
             res.json(result);
         })
 
-        app.put('/users/admin', async (req, res) => {
+        app.put('/users/admin', verifyToken, async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
             const updateDoc = {
